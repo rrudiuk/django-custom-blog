bb64d518bb0db7dceaec4de6e78d854816071d06
rrudiuk <rus.barik@gmail.com>
2020-06-22 21:42:44 +0300

    Added Search through website posts

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 # class PostListView(ListView):
 # 	queryset = Post.published.all()
@@ -91,3 +93,23 @@ def post_share(request, post_id):
 		form = EmailPostForm()
 
 	return render(request, 'blog/post/share.html', {'post': post,'form': form, 'sent': sent})
+
+def post_search(request):
+	form = SearchForm()
+	query = None
+	results = []
+
+	if 'query' in request.GET:
+		form = SearchForm(request.GET)
+
+		if form.is_valid():
+			query = form.cleaned_data['query']
+			# results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+			search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+			search_query = SearchQuery(query)
+			# results = Post.published.annotate(search=search_vector,rank=SearchRank(search_vector, 
+			# 	search_query)).filter(rank__gte=0.2).order_by('-rank')
+			results = Post.published.annotate(similarity=TrigramSimilarity('title', 
+				query),).filter(similarity__gt=0.1).order_by('-similarity')
+
+	return render(request,'blog/post/search.html', {'form': form, 'query': query, 'results': results})
